4960911f04e5bffc824b042abe632d5cc050b146
Hissa <example@example.com>
2018-10-17 16:37:32 +0900

    working

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-class TokenProvider{
+class ConfigurationProvider{
     constructor(){
         this.config = require("./config.json");
     }
@@ -10,24 +10,58 @@ class TokenProvider{
     get secret(){
         return this.config.client_secret;
     }
+
+    get channelNames(){
+        return this.config.channel_names;
+    }
+
+    get templateString(){
+        return this.config.template_string;
+    }
+
+    get trigegr(){
+        return this.config.trigger_strings;
+    }
+
+    get randomMin(){
+        return this.config.random_min;
+    }
+
+    get randomMax(){
+        return this.config.random_max;
+    }
+}
+
+class DumyConfigurationProvider{
+    constructor(){
+        this.id = "";
+        this.secret = "";
+        this.channelNames = [];
+        this.templateString = "";
+        this.trigger = "";
+        this.randomMax = 0;
+        this.randomMin = 0;
+    }
 }
 
 class Random{
-    next(max){
-        return Math.floor(Math.random() * Math.floor(max));
+    // next(max){
+    //     return Math.floor(Math.random() * Math.floor(max));
+    // }
+    next(min, max){
+        return Math.floor(Math.random() * Math.floor(max + min)) - min;
     }
 }
 
 class App{
-    constructor(tokenProvider){
+    constructor(configurationProvider){
         let discord = require("discord.js");
         this.client = new discord.Client();
         this.random = new Random();
-        this.provider = tokenProvider;
-        this.initialize();
+        this.provider = configurationProvider;
     }
 
-    initialize(){
+    run(){
         this.client.on("message", msg => {
             if (msg.channel.name != "ワードウルフ") {
                 return;
@@ -45,5 +79,6 @@ class App{
     }
 }
 
-const app = new App(new TokenProvider());
-console.log("done");
+const app = new App(new DumyConfigurationProvider());
+app.run();
+console.log("running");
